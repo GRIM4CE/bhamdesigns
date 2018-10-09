367a35d5b1350188039afce98dcd8116984b0c65
Jon Leibham <jleibham@gmail.com>
2018-10-09 18:32:11 -0500

    finished up work on the original init

--- src/components/bio.vue
+++ src/components/bio.vue
@@ -8,6 +8,14 @@
       <p>Jon Leibham is a Chicago-based freelance Graphic/Web Designer. He has worked on many things great and small. He takes pride in staying sharp, always looking to learn a new skill. He primarily works as UX/UI front-end web designer but has taken on many other roles over the years. His workflow takes place mainly in a node.js environment allowing him to deliver compressed, clean, and correct code to client or team. When he's not locked in the void, he enjoys camping, cooking, painting, and listening to music.</p>
     </div>
 
+    <ul class="sidebar-media-list">
+      <li class="sidebar-media-list-item" v-for="({icon, link}, index) in media">
+        <a class="sidebar-media-link" :href="link" target="_blank">
+          <fa :icon="icon"/>
+        </a>
+      </li>
+    </ul>
+
   </aside>
 </template>
 
@@ -16,6 +24,22 @@ export default {
   data() {
     return {
       isActive: false,
+      media: [{
+        icon: 'envelope',
+        link: 'mailto:jleibham@gmail.com',
+      }, {
+        icon: ['fab', 'linkedin-in'],
+        link: 'https://www.linkedin.com/in/jon-leibham-a29b93105/',
+      }, {
+        icon: ['fab', 'github'],
+        link: 'https://github.com/GRIM4CE',
+      }, {
+        icon: ['fab', 'npm'],
+        link: 'https://www.npmjs.com/~grim4ce',
+      }, {
+        icon: ['fab', 'stack-overflow'],
+        link: 'https://stackoverflow.com/users/3561677/jleibham',
+      }],
     }
   },
   methods: {
@@ -27,9 +51,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  @import '../sass/variables.scss';
+@import '../sass/variables.scss';
 
-  .sidebar {
+.sidebar {
     display: none;
     position: fixed;
     transform: translateX(-350px);
@@ -38,18 +62,22 @@ export default {
     height: calc(100vh - 65px);
     background-color: rgba($black, .98);
     z-index: 2;
-    transition: transform .8s;
+    transition: transform 0.8s;
 
     &.active {
-      transform: translateX(0);
+        transform: translateX(0);
 
-      .sidebar-tab {
-        background-color: $secondary400;
-      }
+        .sidebar-tab {
+            background-color: $secondary400;
+        }
     }
-  }
 
-  .sidebar-tab {
+    @include desktop {
+      display: initial;
+    }
+}
+
+.sidebar-tab {
     position: absolute;
     display: flex;
     justify-content: center;
@@ -60,33 +88,46 @@ export default {
     right: -50px;
     width: 50px;
     height: 130px;
-    transition: background-color .5s ease-in;
+    transition: background-color 0.5s ease-in;
     background-color: $primary400;
     cursor: pointer;
 
     &:hover {
-      background-color: $secondary400;
+        background-color: $secondary400;
     }
-  }
+}
 
-  .sidebar-tab-icon {
+.sidebar-tab-icon {
     color: $white;
     font-size: 24px;
-  }
+}
 
-  .sidebar-bio-content {
+.sidebar-bio-content {
     padding: 1.5rem;
     color: $white;
     font-weight: 300;
-  }
+}
 
-  .sidebar-bio-headshot {
+.sidebar-bio-headshot {
     max-width: 100%;
-  }
+    border-radius: 2px;
+}
 
-  @media all and (min-width: 62.5rem) {
-    .sidebar {
-      display: initial;
-    }
+.sidebar-media-list {
+  display: flex;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 1rem 2rem;
+  justify-content: space-around;
+}
+
+.sidebar-media-link {
+  color: $white;
+  font-size: 18px;
+
+  &:hover {
+    color: $secondary400;
   }
+}
 </style>
--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,8 +1,11 @@
 <template>
-<section>
-  <h1>{{gallery.title}}</h1>
+<section class="gallery-section">
+  <h1 class="gallery-title">{{gallery.title}}</h1>
   <div class="gallery-container">
-    <img v-for="(image, index) in gallery.images" :key="index" :src="'../static/galleries' + image" alt="">
+    <img class="gallery-img" v-for="(image, index) in gallery.images" :key="index" :src="'../static/galleries' + image" alt="">
+  </div>
+  <div class="gallery-description-container">
+    <p class="gallery-description">{{gallery.description}}</p>
   </div>
 </section>
 </template>
@@ -13,16 +16,56 @@ import store from '@/store'
 export default {
   computed: {
     gallery() {
-       return store.state.gallery
+      return store.state.gallery
     },
   },
   created() {
     this.loading = true
     store.dispatch('fetchGallery', this.$route.params.id)
-      .then(() => { this.loading = false })
+      .then(() => {
+        this.loading = false
+      })
   },
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+@import '../sass/variables.scss';
+
+.gallery-section {
+    display: flex;
+    flex-wrap: wrap;
+    height: calc(100vh - 106px);
+}
+
+.gallery-title {
+    margin: 3rem 3rem 0;
+}
+
+.gallery-container {
+    display: flex;
+    align-items: center;
+    height: 60%;
+    margin: 1rem 3rem;
+    overflow: auto;
+}
+
+.gallery-img {
+    display: inline-block;
+    max-height: 100%;
+    margin: 0 1rem;
+}
+
+.gallery-description-container {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    background-color: $primary200;
+}
+
+.gallery-description {
+  margin: 1rem 1.5rem;
+  color: $white;
+  font-size: 16px;
+}
 </style>
