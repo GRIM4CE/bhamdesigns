3336f072ac88662d21bb93971c2e6615b368453a
Jon Leibham <jleibham@gmail.com>
2018-10-11 15:16:41 +0000

    fixes to server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+var express = require('express')
+var app = express()
+var path = require('path')
+var history = require('connect-history-api-fallback')
 
+app.use(history())
 app.use(express.static('dist'))
 
-app.listen(8080);
+app.listen(8080)
