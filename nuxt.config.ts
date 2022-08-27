6c4e91ea0e2e57281eb8ec7bbaa8934b2dd08109
jleibham <jleibham@gmail.com>
2022-08-27 13:06:51 -0500

    moves css file into nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['~/modules/sitemap'],
+  head: {
+    link: [
+      { rel: 'stylesheet', type: 'text/css', href: './assets/css/main.css' },
+    ]
+  },
   sitemap: {
     hostname: 'https://bhamdesigns.com',
   },
