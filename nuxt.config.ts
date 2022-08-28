c2fa13187f5665c7e9b36214a10a576fcd31505d
jleibham <jleibham@gmail.com>
2022-08-28 11:32:56 -0500

    updates to styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,9 @@ export default defineNuxtConfig({
   head: {
     link: [
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
-      { rel: "preconnect", href: "https://bhamdesigns.imgix.net" }
+      { rel: "preconnect", href: "https://bhamdesigns.imgix.net" },
+      { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: true },
+      { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" }
     ]
   },
   css: [
